b4fd371ae8279eeddbe6d776cb7c7a2bd3def430
Levi Eber <gblevieber@gmail.com>
2024-11-19 23:29:03 -0300

    feat: add history-substring-search and autosuggestions on zsh

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -3,12 +3,15 @@ bindkey -e
 bindkey '^p' history-search-backward
 bindkey '^n' history-search-forward
 bindkey '^[w' kill-region
+bindkey '^[[A' history-substring-search-up # or '\eOA'
+bindkey '^[[B' history-substring-search-down # or '\eOB'
 
 # History
 HISTSIZE=1000
 HISTFILE=~/.zsh_history
 SAVEHIST=$HISTSIZE
 HISTDUP=erase
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
 setopt appendhistory
 setopt sharehistory
 setopt hist_ignore_space
--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -2,7 +2,10 @@
 
 zinit light zsh-users/zsh-syntax-highlighting
 zinit light zsh-users/zsh-completions
+zinit light zsh-users/zsh-autosuggestions
 zinit light Aloxaf/fzf-tab
+zinit load zsh-users/zsh-history-substring-search
+zinit ice wait atload_history_substring_search_config
 
 # Loads completions
 autoload -U compinit && compinit -C
